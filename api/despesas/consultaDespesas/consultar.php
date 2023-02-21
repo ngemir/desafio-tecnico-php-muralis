f516de91589f0aed072a5779176f569b2cdfb399
Emir Takayama <ngemir@gmail.com>
2023-02-21 11:04:36 -0300

    Gif das requisições

--- api/despesas/consultaDespesas/consultar.php
+++ api/despesas/consultaDespesas/consultar.php
@@ -207,6 +207,7 @@ if ($_GET['formato'] == 'pdf') {
     }
   }
 
+  require('../../connection/connection.php');
   $conexao = new ConexaoBD();
   // Conecta no banco de dados (A Lib funciona com mysqli_connect)
   $link = mysqli_connect($conexao->getHospedagem(), $conexao->getUsuario(), $conexao->getSenha(), $conexao->getBancoDeDados());
